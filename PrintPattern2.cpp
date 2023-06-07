45c48fec285d8aed152c7eb3650d8d8838bb5dc8
Manish Kumar <manishkumar80819@gmail.com>
2023-06-07 23:00:52 +0530

    Add files via upload

---
+++ PrintPattern2.cpp
@@ -0,0 +1,28 @@
+/*Print Pattern 2
+n=5
+*
+**
+***
+****
+*****
+
+ */
+#include<bits/stdc++.h>
+using namespace std;
+void printpattern2(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+int main ()
+{
+    int n;
+    cin>>n;
+    printpattern2(n);
+}
